c445dad95982ea3c70ccc5a03c61500d71fa385f
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 20:48:33 +0200

    update

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,6 +1,9 @@
+import React from "react"
 import { storyblokEditable } from "@storyblok/react";
-
-const Feature = ({ blok }) => {
+interface Props {
+   blok: any;
+}
+const Feature:React.FC<Props> = ({ blok }) => {
   return (
     <div className="py-2" {...storyblokEditable(blok)} key={blok._uid}>
       <h2 className="text-lg"> {blok.name} </h2>
