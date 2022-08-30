e8523c586ed935f2c207389387264d9f7726f212
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 23:40:47 +0200

    upate

--- components/SectionWithImage.tsx
+++ components/SectionWithImage.tsx
@@ -3,29 +3,32 @@ import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 import cn from "classnames";
 import { BaseStoryBlockProps } from "../types/global";
 import Image from "next/image";
+import Container from "./Container";
 
 const SectionWithImage: React.FC<BaseStoryBlockProps> = React.memo(
   ({ blok }) => {
     console.log("SectionWithImage", blok);
     const imageUrl = blok.image.filename;
     return (
-      <div
-        className={cn("grid grid-cols-1 md:grid-cols-2 bg-[#F0F0F0]")}
-        {...storyblokEditable(blok)}
-      >
-        <div className="w-full h-full px-5">
-          <div className="w-full h-full relative">
-            <Image src={imageUrl} alt="" layout="fill" objectFit="cover" />
+      <Container>
+        <div
+          className={cn("grid grid-cols-1 md:grid-cols-2 bg-[#F0F0F0]")}
+          {...storyblokEditable(blok)}
+        >
+          <div className="w-full h-full pr-5">
+            <div className="w-full h-full relative">
+              <Image src={imageUrl} alt="" layout="fill" objectFit="cover" />
+            </div>
+          </div>
+          <div className="w-full h-full px-5 py-7 bg-[#F0F0F0]">
+            {blok.blocks.map((blok: any) => (
+              <div key={blok._uid} className="flex-auto px-6">
+                <StoryblokComponent blok={blok} />
+              </div>
+            ))}
           </div>
         </div>
-        <div className="w-full h-full px-5 py-7 bg-[#F0F0F0]">
-          {blok.blocks.map((blok: any) => (
-            <div key={blok._uid} className="flex-auto px-6">
-              <StoryblokComponent blok={blok} />
-            </div>
-          ))}
-        </div>
-      </div>
+      </Container>
     );
   }
 );
