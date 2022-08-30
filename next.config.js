56843f5f1ad2f580ce007ec34d4e74343c0b085a
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 22:41:51 +0200

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "media.graphassets.com",
+      "images.unsplash.com",
+      "picsum.photos",
+      "images.pexels.com",
+      "www.pexels.com",
+      "i.imgur.com",
+      "digitalebildungfueralle.org",
+      "a.storyblok.com"
+    ],
+  },
 }
 
 module.exports = nextConfig
