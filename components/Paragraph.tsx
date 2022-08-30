21bf5e097dd6b790eb19fba0cf4450e82ace276d
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 23:58:52 +0200

    upate

--- components/Paragraph.tsx
+++ components/Paragraph.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import cn from "classnames";
+import StoryblokEditableWrapper from "./StoryblokEditableWrapper";
+import { BaseStoryBlockProps } from "../types/global";
 interface Props {}
-const Paragraph: React.FC<Props> = React.memo(({}) => {
-  return <div className={cn("")}> Paragraph </div>;
+const Paragraph: React.FC<BaseStoryBlockProps> = React.memo(({ blok }) => {
+  return (
+    <StoryblokEditableWrapper blok={blok}>
+      <p className="">{blok.value}</p>
+    </StoryblokEditableWrapper>
+  );
 });
 export default Paragraph;
