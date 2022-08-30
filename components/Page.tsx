660aebb6746cd7b84e51d6c098fe1d88885ab590
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 20:52:05 +0200

    update

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-
-const Page = ({ blok }) => (
+interface Props {
+  blok: any;
+}
+const Page:React.FC<Props> = ({ blok }) => (
   <main className="px-6" {...storyblokEditable(blok)} key={blok._uid}>
-    {blok.body.map((nestedBlok) => (
+    {blok.body.map((nestedBlok:any) => (
       <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
     ))}
   </main>
