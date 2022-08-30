633d32d919a1b9cf6642e5c6bb22e58c95f1d6ae
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 20:50:55 +0200

    update:

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,11 @@
+import React from "react"
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-
-const Grid = ({ blok }) => (
+interface Props {
+  blok: any;
+}
+const Grid:React.FC<Props> = ({ blok }) => (
   <ul className="flex py-8 mb-6" {...storyblokEditable(blok)} key={blok._uid}>
-    {blok.columns.map((blok) => (
+    {blok.columns.map((blok:any) => (
       <li key={blok._uid} className="flex-auto px-6">
         <StoryblokComponent blok={blok} />
       </li>
