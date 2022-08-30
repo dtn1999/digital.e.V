9f8ea7807d6e17d4f75dbee39987c0aea48a3dca
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 23:48:37 +0200

    update

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cn from "classnames";
-import { BaseStoryBlockProps } from "../types/global";
-const Container: React.FC<BaseStoryBlockProps> = React.memo(({ children }) => {
+import { BaseProps, BaseStoryBlockProps } from "../types/global";
+const Container: React.FC<BaseProps> = React.memo(({ children }) => {
   return <div className={cn("max-w-7xl mx-auto")}>{children}</div>;
 });
 export default Container;
---
+++ components/Paragraph.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import cn from "classnames";
+interface Props {}
+const Paragraph: React.FC<Props> = React.memo(({}) => {
+  return <div className={cn("")}> Paragraph </div>;
+});
+export default Paragraph;
