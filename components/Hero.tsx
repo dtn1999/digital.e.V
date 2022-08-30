f46035b65617f593dc3e84dabd3e8628538b9922
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 23:11:05 +0200

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { storyblokEditable } from "@storyblok/react";
+import { BsArrowRight } from "react-icons/bs";
 import cn from "classnames";
 import ReactPlayer from "react-player";
 interface Props {
   blok: any;
 }
 const HeroSection: React.FC<Props> = React.memo(({ blok }) => {
-  console.log("block properties ", blok);
+  const videoUrl = blok.video.filename;
+  const heroTitle = blok.title;
   return (
     <div
       className={cn("relative w-hull h-[70%] bg-sky-800")}
@@ -27,11 +29,26 @@ const HeroSection: React.FC<Props> = React.memo(({ blok }) => {
             },
           },
         }}
-        url="https://player.vimeo.com/external/541751571.hd.mp4?s=53b19cc6a2c6d411ce5856b33280c74d85094898&profile_id=175&oauth2_token_id=57447761"
+        url={videoUrl}
         loop
         muted
         playing
       />
+      <div className="bg-black/60 absolute items-center top-0 left-0 w-full h-full z-10 flex">
+        <div className="relative w-full max-w-7xl mx-auto">
+          <h1 className="text-white text-[62px] max-w-[640px] font-bold mb-10">
+            {heroTitle}
+          </h1>
+          <button className="flex items-center text-white bg-primary px-10 py-5 text-lg font-normal rounded-[200px] group transition-all duration-700">
+            <span className="">
+              <BsArrowRight className="text-4xl group-hover:translate-x-5 transition-all duration-700" />
+            </span>
+            <span className="first-letter:uppercase mx-5 group-hover:translate-x-2 transition-all duration-700">
+              Learn more
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 });
