928526049611fcaff9ad2f8567fe5ef15889b117
dtn1999 <tchekamboudanyls@gmail.com>
2022-08-30 22:38:09 +0200

    update

--- components/Page.tsx
+++ components/Page.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 import { BaseStoryBlockProps } from "../types/global";
 
-
-const Page:React.FC<BaseStoryBlockProps> = ({ blok }) => (
-  <main className="px-6" {...storyblokEditable(blok)} key={blok._uid}>
-    {blok.body.map((nestedBlok:any) => (
+const Page: React.FC<BaseStoryBlockProps> = ({ blok }) => (
+  <main className="w-full h-full" {...storyblokEditable(blok)} key={blok._uid}>
+    {blok.body.map((nestedBlok: any) => (
       <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
     ))}
   </main>
