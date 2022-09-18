1ec8cecc2ab8fbfcc13ae9f44dc94ec10880c5f4
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 14:53:31 +0200

    update

--- components/Blocks/MultiColumn/index.tsx
+++ components/Blocks/MultiColumn/index.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import cn from "classnames";
-import { BaseProps } from "@app/types";
 import Column from "./Column";
 
-interface Props extends BaseProps {
-  columns: any[];
-  numberOfColumnsOnDesktop: number;
-  numberOfColumnsOnMobile: number;
-}
-
-const MultiColumn: React.FC<Props> = React.memo(({ children, ...props }) => {
+const MultiColumn: React.FC<any> = React.memo(({ children, ...props }) => {
   const {
     columns = [],
   } = props;
 
   return (
     <div className={cn("w-full grid sm:grid-cols-1 gap-8 md:grid-cols-2")}>
-      {columns.map((column, index) => (
+      {columns.map((column:any, index:number) => (
         <Column key={index} {...column} />
       ))}
     </div>
