81257499b72b41898d8cf723a02d4460fbd75fff
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 14:48:12 +0200

    update

--- components/Blocks/Form/FormInput.tsx
+++ components/Blocks/Form/FormInput.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import cn from "classnames";
-import { BaseProps } from "@app/types";
-import { FormInputFragment } from "@app/types/graphql";
 import { useFormContext } from "react-hook-form";
 
-interface Props
-  extends BaseProps,
-    Omit<Required<FormInputFragment>, "__typename" | "id" | "label"> {
-  error?: any;
-}
-
-const FormInput: React.FC<Props> = React.memo(({ className, ...props }) => {
+const FormInput: React.FC<any> = React.memo(({ className, ...props }) => {
   const { placeholder, name, required, type } = props;
   const { register } = useFormContext();
 
