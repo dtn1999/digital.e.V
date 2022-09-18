e33368f7fa897f8fadbbfcbd5fadf8f2acf7801e
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 14:46:24 +0200

    update

---
+++ pages/[[...slug]].tsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+ import cn from 'classnames';
+interface Props {
+    
+}
+const Page:React.FC<Props> = React.memo(({}) => {
+     return <div className={cn('')}> Page </div>
+})
+export default Page
