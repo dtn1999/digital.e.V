7ac8db1a88a42bcc69cf85204f9a15a8d5ddcac2
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 15:15:35 +0200

    update

--- components/sections/Hero/HeroManager.tsx
+++ components/sections/Hero/HeroManager.tsx
@@ -1,27 +1,20 @@
 import React from "react";
-import { BannerFragment, CarouselFragment, PageHero } from "@app/types/graphql";
 import dynamic from "next/dynamic";
 
 // some dynamic imports
-const Carousel = dynamic(() => import("../../../components/sections/Carousel"));
+const Carousel = dynamic(() => import("./Carousel"));
 const Banner = dynamic(() => import("./Banner"));
 
 // internal component types
-type HeroTypenameUnion = Required<Pick<PageHero, "__typename">>["__typename"];
 
-interface Props {
-  typename: HeroTypenameUnion;
-  data: CarouselFragment | BannerFragment;
-}
-
-const HeroManager: React.FC<Props> = React.memo(({ typename, data }) => {
+const HeroManager: React.FC<any> = React.memo(({ typename, data }) => {
   switch (typename) {
     case "Carousel":
-      return (
-        <Carousel key={data.id} slides={(data as CarouselFragment).slides} />
-      );
+      return <Carousel key={data.id} slides={(data as any).slides} />;
     case "Banner":
-      return <Banner key={data.id} {...(data as BannerFragment)} />;
+      return <Banner key={data.id} {...(data as any)} />;
+    default:
+      return <React.Fragment></React.Fragment>;
   }
 });
 export default HeroManager;
