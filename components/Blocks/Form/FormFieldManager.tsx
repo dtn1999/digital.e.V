d2c4f4535805e0524fbce30a6442e89ab409bd1f
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 14:47:10 +0200

    update

--- components/Blocks/Form/FormFieldManager.tsx
+++ components/Blocks/Form/FormFieldManager.tsx
@@ -11,21 +11,14 @@ const FormFieldSwitcher: React.FC<any> = React.memo(
   ({ typename, __typename, error, ...field }) => {
     switch (typename || __typename) {
       case "FormInput":
-        //TODO: fix type for data
-        //@ts-ignore
         return <FormInput key={field.id} {...field} error={error} />;
       case "FormTextarea":
-        //TODO: fix type for data
-        //@ts-ignore
         return <FormTextarea key={field.id} {...field} error={error} />;
       case "FormRadio":
-        //@ts-ignore
         return <FormRadio key={field.id} {...field} error={error} />;
       case "FormCheckbox":
-        // @ts-ignore
         return <FormCheckbox key={field.id} {...field} error={error} />;
       case "FormSelect":
-        // @ts-ignore
         return <FormSelect key={field.id} {...field} error={error} />;
       default:
         return null;
