4a0109b181c4472928740aff85674f2188e7d1bf
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 15:42:32 +0200

    update

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,9 +1,28 @@
-import React from 'react'; 
- import cn from 'classnames';
-interface Props {
-    
-}
-const Page:React.FC<Props> = React.memo(({}) => {
-     return <div className={cn('')}> Page </div>
-})
-export default Page
+import React from "react";
+import { getStoryblokApi } from "@storyblok/react";
+import cn from "classnames";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface Props {}
+
+const Page: React.FC<Props> = React.memo(({}) => {
+  return <div className={cn("")}> Page </div>;
+});
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {},
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await getStoryblokApi().get("cdn/stories", {
+    version: "draft",
+  });
+  console.log(response)
+  return {
+    paths: [{ params: { slug: [""] } }],
+    fallback: false,
+  };
+};
+export default Page;
