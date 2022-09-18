5c0728fdc4c2ccde87694395506e1a62a32a2201
dtn1999 <tchekamboudanyls@gmail.com>
2022-09-18 15:28:03 +0200

    update

---
+++ utils/envVariables.ts
@@ -0,0 +1,10 @@
+
+export const GRAPHCMS_ENDPOINT = String(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT)
+export const GRAPHCMS_TOKEN = String(process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN)
+export const GRAPHCMS_TOKEN_PREVIEW = String(process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN_PREVIEW)
+export const UNSPLASH_TOKEN = String(process.env.NEXT_PUBLIC_UNSPLASH_TOKEN)
+export const PEXELS_TOKEN = String(process.env.NEXT_PUBLIC_PEXELS_TOKEN)
+export const GRAPHCMS_MUTATION_TOKEN = String(process.env.NEXT_PUBLIC_HYGRAPH_MUTATION_TOKEN)
+export const SITE_SECRET_KEY = String(process.env.NEXT_PUBLIC_SITE_SECRET_KEY)
+export const SITE_KEY = String(process.env.NEXT_PUBLIC_SITE_KEY)
+export const GOOGLE_ANALYTICS_ID = String(process.env.NEXT__PUBLIC_GOOGLE_ANALYTICS_ID)
