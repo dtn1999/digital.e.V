7445ece1d4c55143ed39c3d4e57f4c96a7255381
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-03 21:27:22 +0200

    update

--- components/sections/Hero/HeroManager.tsx
+++ components/sections/Hero/HeroManager.tsx
@@ -8,13 +8,20 @@ const Banner = dynamic(() => import("./Banner"));
 // internal component types
 
 const HeroManager: React.FC<any> = React.memo(({ typename, data }) => {
-  switch (typename) {
-    case "Carousel":
-      return <Carousel key={data.id} slides={(data as any).slides} />;
-    case "Banner":
-      return <Banner key={data.id} {...(data as any)} />;
-    default:
-      return <React.Fragment></React.Fragment>;
-  }
+  const heroComponent = React.useMemo(() => {
+    switch (typename) {
+      case "Carousel":
+        return <Carousel key={data.id} slides={(data as any).slides} />;
+      case "Banner":
+        return <Banner key={data.id} {...(data as any)} />;
+      default:
+        return (
+          <React.Fragment>
+            <h1>Hero Manager</h1>
+          </React.Fragment>
+        );
+    }
+  }, [typename, data]);
+  return <div className="h-full w-full bg-sky-500">{heroComponent}</div>;
 });
 export default HeroManager;
