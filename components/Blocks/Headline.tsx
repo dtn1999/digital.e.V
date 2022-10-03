3d6d90f0a0182c55c28e4c9ae4e06f4ffe4633ad
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-03 22:23:38 +0200

    update

--- components/Blocks/Headline.tsx
+++ components/Blocks/Headline.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import cn from "classnames";
+import { BaseProps } from "../../types/global";
 
 function parseHeadlineWord(word: string, index: number) {
   const wordsComponents = word.split(" ").map((item, indexWord) => {
     if (item.startsWith("{") || item.endsWith("}")) {
       return (
-        <span key={indexWord} className="mr-2 text-primary">
+        <span
+          key={indexWord}
+          className={cn({
+            "text-primary": true,
+            "mr-2": indexWord < word.split(" ").length - 1,
+          })}
+        >
           {item.replace("{", "").replace("}", "")}
         </span>
       );
     } else if (item.startsWith("[") || item.endsWith("]")) {
       return (
-        <span key={indexWord} className="mr-2 text-accent">
+        <span
+          key={indexWord}
+          className={cn({
+            "text-accent": true,
+            "mr-2": indexWord < word.split(" ").length - 1,
+          })}
+        >
           {item.replace("[", " ").replace("]", "")}
         </span>
       );
     }
     return (
-      <span key={indexWord} className="mr-2">
+      <span
+        key={indexWord}
+        className={cn({
+          "mr-2": indexWord < word.split(" ").length - 1,
+        })}
+      >
         {item}
       </span>
     );
@@ -35,21 +53,38 @@ function parseHeadline(headline: string) {
   return words.map(parseHeadlineWord);
 }
 
-const Headline: React.FC<any> = React.memo(
+interface Props extends BaseProps {
+  value: string;
+  underline?: boolean;
+  underlineAlign?: "left" | "center" | "right";
+}
+
+const Headline: React.FC<Props> = React.memo(
   ({ value, underline, underlineAlign, className }) => {
     return (
       <h1
         className={cn({
-          "text-3xl font-black md:text-5xl md:leading-11": true,
-          "after:block after:w-5 after:h-[1px] after:mt-4 after:content-[' '] after:bg-accent":
-            underline,
-          "after:mx-auto": underlineAlign === "center",
-          "after:mt-8": underlineAlign && underlineAlign === "left",
-          "after:ml-auto": underlineAlign === "right",
+          "relative text-3xl font-black md:text-5xl md:leading-tight": true,
           [className || ""]: className,
         })}
       >
-        {parseHeadline(value)}
+        <span className="flex flex-wrap items-start">
+          {parseHeadline(value)}
+        </span>
+        {underline && (
+          <span
+            className={cn(
+              {
+                "absolute inset-0 flex items-end": true,
+                "justify-start": underlineAlign === "left",
+                "justify-center": underlineAlign === "center",
+                "justify-end": underlineAlign === "right",
+              }
+            )}
+          >
+            <hr className={cn("bottom-0 h-[2px] w-10 bg-primary")} />
+          </span>
+        )}
       </h1>
     );
   }
