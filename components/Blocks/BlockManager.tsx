9738fed5fe92af0f1f3410fbc1da57fabf85ca88
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-04 12:09:19 +0200

    update

--- components/Blocks/BlockManager.tsx
+++ components/Blocks/BlockManager.tsx
@@ -1,44 +1,43 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-
 const CountUpComponent = dynamic(() => import("./CountUp"));
 const ParagraphComponent = dynamic(() => import("./Paragraph"));
 const AccordionComponent = dynamic(() => import("./Accordion"));
 const TabComponent = dynamic(() => import("./Tab"));
 const MultiColumnComponent = dynamic(() => import("./MultiColumn"));
 
-
-const BlockSwitcher: React.FC<any> = React.memo(
-  ({ typename, data }) => {
-    switch (typename) {
-      case "Accordion":
-        return <AccordionComponent items={data.items} />;
-      case "Tab":
-        return <TabComponent tabs={data.tabs} />;
-      case "MultiColumn":
-        return (
-          <MultiColumnComponent
-            columns={data.columns}
-            numberOfColumnsOnDesktop={data.numberOfColumnsOnDesktop}
-            numberOfColumnsOnMobile={data.numberOfColumnsOnMobile}
-          />
-        );
-      case "CountUp":
-        return (
-          <CountUpComponent end={data.end} className="text-4xl font-bold text-primary" />
-        );
-      case "Paragraph":
-        return (
-          <div className="mt-4 text-lg">
-            <ParagraphComponent value={data.value} />
-          </div>
-        );
-      default:
-        return <React.Fragment></React.Fragment>;
-    }
+export const BlockSwitcher: React.FC<any> = React.memo(({ typename, data }) => {
+  switch (typename) {
+    case "Accordion":
+      return <AccordionComponent items={data.items} />;
+    case "Tab":
+      return <TabComponent tabs={data.tabs} />;
+    case "MultiColumn":
+      return (
+        <MultiColumnComponent
+          columns={data.columns}
+          numberOfColumnsOnDesktop={data.numberOfColumnsOnDesktop}
+          numberOfColumnsOnMobile={data.numberOfColumnsOnMobile}
+        />
+      );
+    case "CountUp":
+      return (
+        <CountUpComponent
+          end={data.end}
+          className="text-4xl font-bold text-primary"
+        />
+      );
+    case "Paragraph":
+      return (
+        <div className="mt-4 text-lg">
+          <ParagraphComponent value={data.value} />
+        </div>
+      );
+    default:
+      return <React.Fragment></React.Fragment>;
   }
-);
+});
 
 const BlockManager: React.FC<any> = React.memo(({ blocks }) => {
   const blocksComponents = React.useMemo(() => {
