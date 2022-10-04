005fddb6f25b6576386cf2123c7298c81142cb54
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-05 00:13:26 +0200

    update

--- components/Blocks/Form/FormInput.tsx
+++ components/Blocks/Form/FormInput.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import cn from "classnames";
 import { useFormContext } from "react-hook-form";
+import { BaseProps } from "../../../types/global";
 
-const FormInput: React.FC<any> = React.memo(({ className, ...props }) => {
-  const { placeholder, name, required, type } = props;
+interface Props extends BaseProps {
+  label: string;
+  placeholder: string;
+  name: string;
+  required: boolean;
+  type?: HTMLInputElement["type"];
+}
+
+const FormInput: React.FC<Props> = React.memo(({ className, ...props }) => {
+  const { placeholder, name, required, type, label } = props;
   const { register } = useFormContext();
 
   return (
     <div className="flex flex-col w-full mx-2">
-      <p className="font-medium">Vorname</p>
+      <p className="font-medium">{label}</p>
       <input
         {...register(name, { required: required ? true : false })}
+        placeholder={placeholder}
+        type={type}
         className="p-[10px] bg-[rgba(0,0,0,.04)] rounded-sm"
       />
     </div>
