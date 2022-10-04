02a711771b104c45f69ef31849b07dda8c16d15d
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-04 13:41:35 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,8 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./features/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/features/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
