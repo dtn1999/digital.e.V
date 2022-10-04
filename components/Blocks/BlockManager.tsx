66a91011f5d06ab2dddbf1501ef78c4092b21104
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-04 12:25:12 +0200

    update

--- components/Blocks/BlockManager.tsx
+++ components/Blocks/BlockManager.tsx
@@ -1,49 +1,12 @@
 import React from "react";
-import dynamic from "next/dynamic";
-
-const CountUpComponent = dynamic(() => import("./CountUp"));
-const ParagraphComponent = dynamic(() => import("./Paragraph"));
-const AccordionComponent = dynamic(() => import("./Accordion"));
-const TabComponent = dynamic(() => import("./Tab"));
-const MultiColumnComponent = dynamic(() => import("./MultiColumn"));
-
-export const BlockSwitcher: React.FC<any> = React.memo(({ typename, data }) => {
-  switch (typename) {
-    case "Accordion":
-      return <AccordionComponent items={data.items} />;
-    case "Tab":
-      return <TabComponent tabs={data.tabs} />;
-    case "MultiColumn":
-      return (
-        <MultiColumnComponent
-          columns={data.columns}
-          numberOfColumnsOnDesktop={data.numberOfColumnsOnDesktop}
-          numberOfColumnsOnMobile={data.numberOfColumnsOnMobile}
-        />
-      );
-    case "CountUp":
-      return (
-        <CountUpComponent
-          end={data.end}
-          className="text-4xl font-bold text-primary"
-        />
-      );
-    case "Paragraph":
-      return (
-        <div className="mt-4 text-lg">
-          <ParagraphComponent value={data.value} />
-        </div>
-      );
-    default:
-      return <React.Fragment></React.Fragment>;
-  }
-});
+import { StoryblokComponent } from "@storyblok/react";
 
 const BlockManager: React.FC<any> = React.memo(({ blocks }) => {
   const blocksComponents = React.useMemo(() => {
     // @ts-ignore
-    return blocks.map(({ blok }) => (
-      <BlockSwitcher key={blok.id} typename={blok.component} data={blok} />
+
+    return blocks.map((blok) => (
+      <StoryblokComponent blok={blok} key={blok._uid} />
     ));
   }, [blocks]);
   return <React.Fragment>{blocksComponents}</React.Fragment>;
