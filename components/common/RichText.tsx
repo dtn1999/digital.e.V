39946ae37b90e8c458ef23eca0f721e119b6e9f5
dtn1999 <tchekamboudanyls@gmail.com>
2022-10-04 11:59:54 +0200

    update

--- components/common/RichText.tsx
+++ components/common/RichText.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import cn from "classnames";
 import { renderRichText } from "@storyblok/react";
+import { BaseBlokProps } from "../../types/global";
 
-interface Props {}
-const RichText: React.FC<Props> = React.memo(({}) => {
+const RichText: React.FC<BaseBlokProps> = React.memo(({blok}) => {
   return <div className={cn("")}> RichText </div>;
 });
 export default RichText;
