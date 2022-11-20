0606b29a3b82de61688b3886e7eba962df77fee8
dtn1999 <tchekamboudanyls@gmail.com>
2022-11-20 10:37:53 +0100

    update

---
+++ components/Blocks/ImageBlock.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import StoryblokEditable from "../StoryblokEditable";
+import Container from "../common/Container";
+import { StoryblokComponent } from "@storyblok/react";
+import Image from "next/image";
+interface Props {
+  blok: any;
+}
+
+const ImageBlock: React.FC<Props> = React.memo(({ blok }) => {
+  const { image, marginTop, marginBottom } = blok;
+  return (
+    <StoryblokEditable blok={blok}>
+      <div
+        className="relative w-full h-full max-h-[600px] max-w-[380px]"
+        style={{
+          marginTop: Number(marginTop),
+          marginBottom: Number(marginBottom),
+        }}
+      >
+        <Image src={image.filename} alt={image.alt} layout="fill" />
+      </div>
+    </StoryblokEditable>
+  );
+});
+export default ImageBlock;
